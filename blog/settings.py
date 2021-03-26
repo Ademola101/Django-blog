0a60d8957106c520bb7f3aec7e497ccea168124e
Ademola <ademolaogunmokun@yahoo.com>
2021-03-26 20:10:31 +0100

    statifiles

--- blog/settings.py
+++ blog/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_heroku.settings(locals())
